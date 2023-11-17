2eb4fd95adbefe0643a4ee038f53c8970e312ba7
sk-shafi <142564711+sk-shafi@users.noreply.github.com>
2023-11-17 16:18:38 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,16 @@
+public class RightTrianglePattertn
+  {
+    public static void main(strings args[])
+    {
+      int i,j,row=6;
+      for(i<0;i<row;i++)
+        {
+          for(j=0;j<=i;j--)
+            {
+              System.out.println("*");
+            }
+          System.out.print();
+        }
+    }
+  }
+    
